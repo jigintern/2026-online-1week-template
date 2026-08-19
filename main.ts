1bbe22aea1717c5d64d71888842cb89f40f62b76
t-suzuki <t-suzuki@jig.jp>
2026-08-19 11:15:00 +0900

    👍 denoからviteに移行

--- main.ts
+++
@@ -1,9 +0,0 @@
-import { serveDir } from "@std/http/file-server";
-
-Deno.serve((request) =>
-  serveDir(request, {
-    fsRoot: "public",
-    headers: ["Cache-Control: no-store"],
-    showIndex: true,
-  })
-);
